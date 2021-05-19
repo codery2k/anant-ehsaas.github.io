fb51dc50fc676e13e4f0211b2c29f72669c895bf
Saurabh Miglani <saurabhmiglani@foliosolutions.in>
2021-05-20 00:06:50 +0530

    Refactor vision component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
-import { VisionComponent } from './vision/vision.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,9 +12,17 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { DocumentationComponent } from './about-us/documentation/documentation.component';
 import { FinancialComponent } from './about-us/financial/financial.component';
+import { VisionComponent } from './about-us/vision/vision.component';
 
 @NgModule({
-  declarations: [AppComponent, VisionComponent, HeaderComponent, AboutUsComponent, DocumentationComponent, FinancialComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AboutUsComponent,
+    DocumentationComponent,
+    FinancialComponent,
+    VisionComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
